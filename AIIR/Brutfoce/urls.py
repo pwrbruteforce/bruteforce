ee9580bb59b8a7f1a08f096c980f89de0380a2c5
BatmanWVegas <filip.oliwa@gmail.com>
2017-04-23 20:12:24 +0200

    Rejsetraca
    Roziniecie szkieletu aplikacji o mozliwosc rejestracji nowych
    uzytkownikow.

--- AIIR/Brutfoce/urls.py
+++ AIIR/Brutfoce/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = (
     url(r'^$', views.dashboard, name='dashboard'),
+    url(r'^register/$', views.register, name='register'),
     #login/logout
     url('^', include('django.contrib.auth.urls')),
 )
--- AIIR/Brutfoce/views.py
+++ AIIR/Brutfoce/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -21,6 +21,25 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'Brutforce/login.html', {'form': form})
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+
+        if user_form.is_valid():
+            #Tworzenie nowego obiektu User bez zapisywania go
+            new_user = user_form.save(commit=False)
+            #Ustawienie wybranego hasla
+            new_user.set_password(user_form.cleaned_data['password'])
+            #Zapisanie obiektu User
+            new_user.save()
+            return render(request,
+                          'Brutforce/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'Brutforce/register.html', {'user_form': user_form})
+
 
 @login_required
 def dashboard(request):
